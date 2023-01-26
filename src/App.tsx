e3a06977fdbdf2a33a081815f7dedbc52ddc2631
songkira <thdrlfk93@naver.com>
2023-01-26 14:37:28 +0900

    ModalBasic완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+// import { SetStateAction } from "react";
 import NavBar from "./components/NavBar";
+import Modal from "./components/common/Modal";
+
+// interface PropsTypes {
+//   setModalOpen: React.Dispatch<SetStateAction<boolean>>;
+// }
 
 function App() {
-  return <NavBar/>;
+  return (
+    <>
+      <NavBar />
+      <Modal />
+    </>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
+// import { SetStateAction } from "react";
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById("root") as HTMLElement,
 );
 root.render(
   // <React.StrictMode>
-  <App />
+  <App />,
   // </React.StrictMode>
 );
