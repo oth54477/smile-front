d34fe47e658aa1ec914d27043c88107c74e66396
songkira <thdrlfk93@naver.com>
2023-01-26 16:58:59 +0900

    버튼구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,18 @@
-<<<<<<< HEAD
 // import { SetStateAction } from "react";
-import NavBar from "./components/NavBar";
 import Modal from "./components/common/Modal";
-
-// interface PropsTypes {
-//   setModalOpen: React.Dispatch<SetStateAction<boolean>>;
-// }
-=======
+import ButtonBasic from "./components/common/ButtonBasic";
 import LandingPages from "./pages/LandingPages";
 import NavBar from "./components/navbar/NavBar";
 import Footer from "./components/footers/Footer";
->>>>>>> 7ae00389a0f87a9bacd257b7fe5c7efe6b5c1774
 
 function App() {
   return (
     <>
       <NavBar />
-<<<<<<< HEAD
-      <Modal />
-=======
       <LandingPages />
+      <Modal />
+      {/* <ButtonBasic /> */}
       <Footer />
->>>>>>> 7ae00389a0f87a9bacd257b7fe5c7efe6b5c1774
     </>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-<<<<<<< HEAD
-// import { SetStateAction } from "react";
-=======
 import { theme } from "./theme";
 import { ThemeProvider } from "styled-components";
->>>>>>> 7ae00389a0f87a9bacd257b7fe5c7efe6b5c1774
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
 root.render(
-<<<<<<< HEAD
-  // <React.StrictMode>
-  <App />,
-  // </React.StrictMode>
-=======
   <ThemeProvider theme={theme}>
     <App />
   </ThemeProvider>,
->>>>>>> 7ae00389a0f87a9bacd257b7fe5c7efe6b5c1774
 );
